6cdc47684216d046d758b6b62aca1fd24cf3fd5d
root <root@DESKTOP-CF7P017.localdomain>
2022-04-22 20:59:06 +0800

    add pagerank

---
+++ pagerank/PageRankFinalizeReducer.java
@@ -0,0 +1,25 @@
+package pagerank;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class PageRankFinalizeReducer extends Reducer<Text,Text,Text,Text> {
+	@Override 
+	public void reduce(Text key ,Iterable<Text> values,Context context) throws IOException, InterruptedException {
+		String keystr = key.toString();
+		if(keystr.contains("%09") == false) {
+			for(Text value : values) {
+				context.write(key, value); //we need to write the url & score
+				return;
+			}
+		} else {
+			keystr = keystr.replaceAll("%09", "\t");
+			for(Text value : values) {
+				context.write(new Text(keystr), value); //we need to write the url & score
+				return;
+			}
+		}
+	}	
+}
