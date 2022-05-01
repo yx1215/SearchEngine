af3deaefa700724e25532906690abcdbc171f4af
root <root@DESKTOP-CF7P017.localdomain>
2022-05-02 01:25:21 +0800

    add database files

---
+++ src/main/java/pagerank/URLExtractor.java
@@ -0,0 +1,19 @@
+package pagerank;
+import java.util.LinkedList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class URLExtractor {
+	public static String[] extract(byte[] document) {
+		Document doc = Jsoup.parse(new String(document));
+		Elements link = doc.select("[href]");
+		LinkedList<String> links = new LinkedList<String>();
+		for (Element l : link) 
+			links.add(l.attr("href"));
+		String[] result = new String[links.size()];
+		return links.toArray(result);
+	}
+}
